a39595b923c4ba9f0229149c2b06bd8c23ca0e2d
jsanguinetti <jsangui@gmail.com>
2019-05-30 19:54:26 -0300

    Initial Commit

---
+++ Block Diarios UY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Block Diarios UY
+//
+//  Created by Javier Sanguinetti on 5/30/19.
+//  Copyright © 2019 Javier Sanguinetti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
