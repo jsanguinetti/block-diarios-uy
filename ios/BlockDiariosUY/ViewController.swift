9ef3dbd0d6206ee86364387c97dc1457e5589fa2
jsanguinetti <jsangui@gmail.com>
2019-05-31 18:56:42 -0300

    RN project

---
+++ ios/BlockDiariosUY/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  BlockDiariosUY
+//
+//  Created by Javier Sanguinetti on 5/30/19.
+//  Copyright © 2019 Javier Sanguinetti. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        NSLog("Hello")
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
+
+
+}
+
